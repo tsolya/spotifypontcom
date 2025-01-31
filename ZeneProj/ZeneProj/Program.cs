00cb051321501ed69d4a6550a173f4ba6d3bb47b
tsolya <tardosjuliannaorsolya@turr.hu>
2025-01-31 10:16:40 +0100

    Keszen all a feladat !! (puha diktatura)

--- ZeneProj/ZeneProj/Program.cs
+++ ZeneProj/ZeneProj/Program.cs
@@ -15,41 +15,57 @@ namespace ZeneProj
             AdatBazisKezelo.HallgatottsagSelect("hallgatottsagtabla");
             Fajlbeolvaso.Beolvasas();
             AdatBazisKezelo.UjZene();
-            LegtobbetHallgatott();
-            LeRegebbiLegujjabbZene();
-            LeghallgatottabbMufaj();
+            LegtobbetHallgatott(AdatBazisKezelo.ZenekList, AdatBazisKezelo.HallgatottsagList);
+            LeRegebbiLegujjabbZene(AdatBazisKezelo.ZenekList);
+            LeghallgatottabbMufaj(AdatBazisKezelo.ZenekList, AdatBazisKezelo.HallgatottsagList);
+            Zenekadatai(AdatBazisKezelo.ZenekList);
             Console.ReadKey();
         }
 
-        private static void LeghallgatottabbMufaj()
+        private static void Zenekadatai(List<Zenek> zenelista)
+        {
+            foreach (var v in zenelista)
+            {
+                Console.WriteLine(v.ToString());
+            }
+        }
+
+        private static void LeghallgatottabbMufaj(List<Zenek> zenelista, List<Hallgatottsag> hallista)
         {
             List<string> Mufajlist = new List<string>();
             List<int> HallgatasSzam = new List<int>();
-            foreach (var x in AdatBazisKezelo.ZenekList) {
+            foreach (var x in zenelista) {
                 if (!Mufajlist.Contains(x.Mufaj))
                 {
                     Mufajlist.Add(x.Mufaj);
+                    HallgatasSzam.Add(0);
                 }
             }
-            for (int i = 0; i < AdatBazisKezelo.HallgatottsagList.Count; i++) {
-                HallgatasSzam[Mufajlist.IndexOf(AdatBazisKezelo.ZenekList[i].Mufaj)] += AdatBazisKezelo.HallgatottsagList[i].HallgatasSzam;
+            for (int i = 0; i < hallista.Count; i++) {
+                HallgatasSzam[Mufajlist.IndexOf(zenelista[i].Mufaj)] += hallista[i].HallgatasSzam;
             }
             string leghallgatottabbmufaj = Mufajlist[HallgatasSzam.IndexOf(HallgatasSzam.Max())];
-            Console.WriteLine(leghallgatottabbmufaj);
+            int legtobbhallgatas = HallgatasSzam.Max();
+            
+            Console.WriteLine($"A leghallgatottabb műfaj: {leghallgatottabbmufaj}\n Meghallgatások száma: {legtobbhallgatas}");
+            Vonalak();
+            
 
         }
 
         private static void Vonalak()
         {
+            Console.WriteLine(" ");
             Console.WriteLine("---------------------------------");
+            Console.WriteLine(" ");
         }
 
-        private static void LeRegebbiLegujjabbZene()
+        private static void LeRegebbiLegujjabbZene(List<Zenek> zenelista)
         {
             Zenek legregebbi = new Zenek(150,"asd","ursula","vmi",2025);
             Zenek legujjabb = new Zenek();
 
-            foreach (var v in AdatBazisKezelo.ZenekList) {
+            foreach (var v in zenelista) {
                 if (v.Kiadas < legregebbi.Kiadas)
                 {
                     legregebbi = v;
@@ -65,16 +81,17 @@ namespace ZeneProj
             Vonalak();
         }
 
-        private static void LegtobbetHallgatott()
+        private static void LegtobbetHallgatott(List<Zenek> zenelista, List<Hallgatottsag> hallista)
         {
+            Vonalak();
             Hallgatottsag valtozo = new Hallgatottsag();
-            for (int i = 0; i <AdatBazisKezelo.HallgatottsagList.Count; i++) {
-                if (AdatBazisKezelo.HallgatottsagList[i].HallgatasSzam>valtozo.HallgatasSzam)
+            for (int i = 0; i <hallista.Count; i++) {
+                if (hallista[i].HallgatasSzam>valtozo.HallgatasSzam)
                 {
-                    valtozo = AdatBazisKezelo.HallgatottsagList[i];
+                    valtozo = hallista[i];
                 }
             }
-            Console.WriteLine($"A legtöbbet hallgatott előadó és zenéjének információja: {AdatBazisKezelo.ZenekList[valtozo.ZeneId].ToString()}\n Hallgatottság: {valtozo.HallgatasSzam}");
+            Console.WriteLine($"A legtöbbet hallgatott előadó és zenéjének információja: {zenelista[valtozo.ZeneId].ToString()}\n Hallgatottság: {valtozo.HallgatasSzam}");
             Vonalak();
         }
     }
